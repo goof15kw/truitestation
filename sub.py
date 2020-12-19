e0a650de00506b378ec718ce5e26998cf4caeef9
lmgelinas@gmail.com <lmgelinas@gmail.com>
2020-12-19 03:08:33 +0000

    add env var for key, connect to GPIO, fetch initial value on boot, works!

--- sub.py
+++ sub.py
@@ -5,17 +5,22 @@
 # Import standard python modules.
 import sys
 
+# for, well, GPIOs
+from gpiozero import LED
+
 # Import Adafruit IO MQTT client.
 from Adafruit_IO import MQTTClient
+from Adafruit_IO import Client
 
+import os
 # Set to your Adafruit IO key.
 # Remember, your key is a secret,
 # so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = ''
+ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
 
 # Set to your Adafruit IO username.
 # (go to https://accounts.adafruit.com to find your username)
-ADAFRUIT_IO_USERNAME = ''
+ADAFRUIT_IO_USERNAME = 'Goofencours'
 
 # Set to the ID of the feed to subscribe to for updates.
 FEED_ID = 'chauffe-01'
@@ -45,10 +50,31 @@ def message(client, feed_id, payload):
     # The feed_id parameter identifies the feed, and the payload parameter has
     # the new value.
     print('Feed {0} received new value: {1}'.format(feed_id, payload))
+    if (payload == "ON" ):
+      led.on()
+    else:
+      led.off()
 
+
+# Init led
+led = LED(14)
+#led.off()
+
+# Create an instance of the REST client.
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+chauffe01 = aio.feeds("chauffe-01")
+data = aio.receive(chauffe01.key)
+print('Initial fetch got {0}'.format(data.value))
+# This should forge a call to message... 
+if (data.value == "ON" ):
+  led.on()
+else:
+  led.off()
+aio = None
 
 # Create an MQTT client instance.
-client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY,secure=False)
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY,secure=True)
 
 # Setup the callback functions defined above.
 client.on_connect    = connected
